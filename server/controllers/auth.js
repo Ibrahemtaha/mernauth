ad2402b56da130127ba446a54a00f016ae7d68f2
Ibrahem <bremoo87@hotmail.com>
2020-04-17 14:06:50 +0300

    first commit

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -37,50 +37,50 @@ sgMail.setApiKey(process.env.SENDGRD_API_KEY);
 exports.signup = (req, res) => {
   const { name, email, password } = req.body;
 
-  User.findOne({ email: email }),
-    (err, user) => {
-      if (user) {
-        return res.status(400).json({
-          error: "Email is taken",
-        });
-      }
-      // Token Sign
-      const token = jwt.sign(
-        { name, email, password },
-        process.env.JWT_ACCOUNT_ACTIVATION,
-        { expiresIn: "60m" }
-      );
-      // End Token Sign
-      // Start Email Template
-      const emailData = {
-        from: process.env.EMAIL_FROM,
-        to: email,
-        subject: `Account activation link`,
-        html: `
+  User.findOne({ email: email }, (err, user) => {
+    console.log(user);
+    if (user) {
+      return res.status(400).json({
+        error: "Email is taken",
+      });
+    }
+    // Token Sign
+    const token = jwt.sign(
+      { name, email, password },
+      process.env.JWT_ACCOUNT_ACTIVATION,
+      { expiresIn: "60m" }
+    );
+    // End Token Sign
+    // Start Email Template
+    const emailData = {
+      from: process.env.EMAIL_FROM,
+      to: email,
+      subject: `Account activation link`,
+      html: `
               <h1>Please use the following link to activate your account</h1>
               <p>${process.env.CLIENT_URL}/auth/activate/${token}</p>
               <hr />
               <p>This email may contain sensetive information</p>
               <p>${process.env.CLIENT_URL}</p>
           `,
-      };
-      // End Email Template
-
-      //sgMAIL Start
-      sgMail
-        .send(emailData)
-        .then((sent) => {
-          // console.log('SIGNUP EMAIL SENT', sent)
-          return res.json({
-            message: `Email has been sent to ${email}. Follow the instruction to activate your email `,
-          });
-        })
-        .catch((err) => {
-          // console.log('SIGNUP EMAIL SENT ERROR', err)
-          return res.json({
-            message: err.message,
-          });
-        });
-      // END sgMAIL Start
     };
+    // End Email Template
+
+    //sgMAIL Start
+    sgMail
+      .send(emailData)
+      .then((sent) => {
+        // console.log('SIGNUP EMAIL SENT', sent)
+        return res.json({
+          message: `Email has been sent to ${email}. Follow the instruction to activate your email `,
+        });
+      })
+      .catch((err) => {
+        // console.log('SIGNUP EMAIL SENT ERROR', err)
+        return res.json({
+          message: err.message,
+        });
+      });
+    // END sgMAIL Start
+  });
 };
